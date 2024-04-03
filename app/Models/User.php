31672a6611a5721b7c63fc6bc8f81f312070acfb
bunyaminakcay <bakcay@live.com>
2024-04-03 03:28:54 +0300

    User and Suvscription relations added

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,71 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Tymon\JWTAuth\Contracts\JWTSubject;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Ramsey\Uuid\Uuid;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
+
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property mixed $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Subscription|null $activeSubscription
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Subscription> $subscriptions
+ * @property-read int|null $subscriptions_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @property string|null $phone
+ * @property string $address1
+ * @property string|null $address2
+ * @property string $city
+ * @property string $region
+ * @property string $country
+ * @property string|null $tax_office
+ * @property string|null $tax_number
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAddress1($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAddress2($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCountry($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRegion($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTaxNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTaxOffice($value)
+ * @property string|null $subscriber_id
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereSubscriberId($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable implements JWTSubject {
-    use HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
+
+
 
     /**
      * The attributes that are mass assignable.
@@ -47,5 +105,12 @@ class User extends Authenticatable implements JWTSubject {
 
     public function getJWTCustomClaims() {
         return [];
+    }
+
+    public function subscriptions() {
+        return $this->hasMany(Subscription::class);
+    }
+    public function activeSubscription() {
+        return $this->hasOne(Subscription::class)->where('status', 'active');
     }
 }
